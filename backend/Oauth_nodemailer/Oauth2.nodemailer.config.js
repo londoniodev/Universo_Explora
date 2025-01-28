51f18f6a7852fd2a7701dfd8907a95d5688ff2a3
Cobayaa <jeanzambrano2020@itp.edu.co>
2025-01-28 12:07:09 -0500

    new smtp hostgator and titan email sender

--- backend/Oauth_nodemailer/Oauth2.nodemailer.config.js
+++ backend/Oauth_nodemailer/Oauth2.nodemailer.config.js
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer"
-import { google } from "googleapis"
+// import { google } from "googleapis"
 import { User } from "../models/user.model.js"
 import mongoose from "mongoose"
 import dotenv from 'dotenv'
@@ -8,60 +8,77 @@ import { VERIFICATION_EMAIL_TEMPLATE } from "../Oauth_nodemailer/Oauth_templates
 
 dotenv.config();
 
-const OAuth2 = google.auth.OAuth2
+// const OAuth2 = google.auth.OAuth2
 
-const oauth2Client = new OAuth2(
-    process.env.OAUTH_CLIENT_ID,
-    process.env.OAUTH_CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-);
+// const oauth2Client = new OAuth2(
+//     process.env.OAUTH_CLIENT_ID,
+//     process.env.OAUTH_CLIENT_SECRET,
+//     "https://developers.google.com/oauthplayground"
+// );
 
-oauth2Client.setCredentials({
-    refresh_token: process.env.OAUTH_REFRESH_TOKEN,
-});
+// oauth2Client.setCredentials({
+//     refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+// });
 
-const getAccessToken = async () => {
-    try {
-        const { token } = await oauth2Client.getAccessToken()
-        return token;
-    } catch (error) {
-        console.error('Error obteniendo el access token:', error)
-        return null
-    }
-};
+// const getAccessToken = async () => {
+//     try {
+//         const { token } = await oauth2Client.getAccessToken()
+//         return token;
+//     } catch (error) {
+//         console.error('Error obteniendo el access token:', error)
+//         return null
+//     }
+// };
 
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    // service: 'gmail',
+    // auth: {
+    //     type: 'OAuth2',
+    //     user: process.env.SMTP_EMAIL,
+    //     clientId: process.env.OAUTH_CLIENT_ID,
+    //     clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    //     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    //     accessToken: async () => getAccessToken(),
+    // },
+    // tls: {
+    //     rejectUnauthorized: false
+    // },
+    // NEW SMTP CONFIGURATION
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
     auth: {
-        type: 'OAuth2',
-        user: process.env.SMTP_EMAIL,
-        clientId: process.env.OAUTH_CLIENT_ID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-        accessToken: async () => getAccessToken(),
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
     },
+    secure: process.env.SMTP_SECURE === "true",
     tls: {
         rejectUnauthorized: false
-    },
+    }
 });
 
 
-export const sendVerificationEmail = async (email,userId) => {
-    const verificationToken = crypto.randomInt(100000,999999).toString()
-    const verificationTokenExpiresAt = Date.now() + 15 * 60 * 1000
+export const sendVerificationEmail = async (email, userId) => {
+    try{
+        const verificationToken = crypto.randomInt(100000,999999).toString()
+        const verificationTokenExpiresAt = Date.now() + 15 * 60 * 1000
 
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
-        throw new Error('Invalid userId format')
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            throw new Error('Invalid userId format')
+        }
+
+        const objectId = new mongoose.Types.ObjectId(userId)
+        await User.findByIdAndUpdate(objectId, { verificationToken, verificationTokenExpiresAt })
+
+        const mailOptions = {
+            from: `"Explora Support - NoReply (No Responder)" <${process.env.SMTP_USER}>`,
+            to: email,
+            subject: "Verificación de correo electrónico",
+            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
+        };
+        await transporter.sendMail(mailOptions);
+        console.log(`Correo de verificación enviado a ${email}`);
+    }catch(error){
+        console.error("Error al enviar el correo de verificación:", error);
+        throw new Error("Error al enviar el correo de verificación", error)
     }
-
-    const objectId = new mongoose.Types.ObjectId(userId)
-    await User.findByIdAndUpdate(objectId, { verificationToken, verificationTokenExpiresAt })
-
-    const mailOptions = {
-        from: `"Explora Support - NoReply (No Responder)" <${process.env.SMTP_EMAIL}>`,
-        to: email,
-        subject: "Verificación de correo electrónico",
-        html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
-    };
-    transporter.sendMail(mailOptions)
 }
