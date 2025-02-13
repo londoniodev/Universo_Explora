70494be6cf43647e0e2262f81b5390f45ef9432f
Cobayaa <jeanzambrano2020@itp.edu.co>
2025-02-13 16:42:17 -0500

    video con iframe

--- frontend/src/pages/Home_components/VideoSection.jsx
+++ frontend/src/pages/Home_components/VideoSection.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
 const VideoSection = () => {
-  // Reemplaza este enlace con el enlace directo a tu video en Google Drive
-  const videoUrl = "https://drive.google.com/file/d/1297HN_X2SpuVpMhE2A1sbuYTl7BNAn4c/view?usp=drive_link";
+  // Reemplaza este enlace con el enlace compartido de tu video en Google Drive
+  const videoUrl = "https://drive.google.com/file/d/1297HN_X2SpuVpMhE2A1sbuYTl7BNAn4c/preview";
 
   return (
     <div className="video-section">
       <h2>Reproducción de Video</h2>
       <div className="video-container">
-        <video width="100%" height="auto" controls>
-          <source src={videoUrl} type="video/mp4" />
-          Tu navegador no soporta la reproducción de videos.
-        </video>
+        <iframe
+          src={videoUrl}
+          width="100%"
+          height="400"
+          allow="autoplay"
+          title="Video de Google Drive"
+        ></iframe>
       </div>
     </div>
   );
